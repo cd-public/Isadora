9d02c4bab674b29da2cfd7bb867ea01e0cb69e88
calvin <cd@cs.unc.edu>
2021-01-07 13:07:33 -0500

    adding manual spins 2

--- all.py
+++ all.py
@@ -50,7 +50,7 @@ def make_spinfo(name,header):
 			to_write.write("0==" + reg.replace("[","").replace("]","") + "\n")
 			last = reg
 	to_write.write("shadow_M_AXI_AWADDR_INT == shadow_M_AXI_AWADDR_wire\n"); # custom add
-	to_write.write("shadow_M_AXI_AWADDR_wire >= reg23_w_config\n"); # custom add
+	to_write.write("M_AXI_AWADDR_wire >= reg23_w_config\n"); # custom add
 
 def dump(key,file,nonce):
 	if nonce > 0:
