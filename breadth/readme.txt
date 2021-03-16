961672a0f1c763d7cc867cf2a081726892ad55f5
calvin <cd@cs.unc.edu>
2021-03-16 17:10:45 -0400

    new directory structure, consistent refine

---
+++ breadth/readme.txt
@@ -0,0 +1,48 @@
+Test directory, research plan:
+
+1) Consult 2pass.py generation in main directory, set n = 1.
+
+    This is included in cond_loop_test as r_CLT_TR0_M_AXI_AWADDR_wire*
+
+2) Select one conditionally tainted register, test_reg_n.
+
+    Look at *.spinfo, select a file. Selection criteria, M_AXI_*_wire, break ties alphabetically
+
+3) [Fabricant] Using Tortuga, set test_reg_1 as a taint source using a dummy property.
+
+    Edit iACW_sps.as in local folder found by following do_fabricant in main directory.
+    Verify novel vcd generation using greg assert iACW.vcd
+
+4) [Fabricant -> Local] Convert Tortuga output .vcd to a 2pass.py input .vcd using refine_vcd.py
+
+    Current best practice is on an intermediate work directory on fabricant between Isadora and iterative_build.
+
+5) [Local] Configure and run 2pass.py, with cleanup configured. 
+
+    This directories 2pass has the 2nd pass commenting out, cleanup commented in, and spinfo cleanup commented out. The input also varies.
+
+6) [Local -> Fabricant] Consult 2pass.py generation and repeat loop beginning at step 2 with n = n+1
+
+7) Use get_conds.py to parse the .spinfo files to append a third line of conditionally tainted registers to the *_ts.out file.
+
+8) Compose the *_ts.out into results.txt
+
+    head -n -0 *.out > results.txt
+
+Process Notes:
+
+Only run 2pass up until second miner pass to identifying conditional taints without incurring the second pass mining cost.
+
+Name VCD's using trn for test_reg_n, maintain this terminology through the loop.
+
+Rename the internal 2pass spinfos to retain the list of conditional taints at each stage - consider encoding here with *_ts.out.
+
+Performance Notes:
+
+Tortuga stage takes about 160 seconds
+
+Daikon stage takes about 10 seconds.
+
+All other stages about a second or less.
+
+Transmitting VCD's from UCSD to my local Daikon environment has some non-zero time cost.
---
+++ tortuga/refine_vcd.py
@@ -0,0 +1,97 @@
+def refine(name):
+
+## refine vcd
+
+	vcd_in = open(name + ".vcd","r")
+	vcd_out = open("r_" + name + ".vcd","w")
+	
+	## inits
+	step0 = True 						# tracks location in the VCD
+	step1 = False
+	step2 = False
+	step3 = False
+	orig_design_regs = set()			# tracks verilog names of origianl design
+	refined_vars = []					# tracks VCD internal names of original design
+	cache = ""							# tracks timing info
+	cache_live = False
+	tag = ""							# internal line read variable for vcd encoding
+	active = False						# tracks if copying is active
+	
+	for line in vcd_in:
+	
+## step 0: copy header
+
+		if step0:
+			if "scope" in line:
+				step0 = False
+				step1 = True
+			else:
+				vcd_out.write(line)
+
+## step 1: copy original design state
+
+		## assume all state in "u0" module
+
+		if step1:
+			
+			if "module myHWtask" in line:
+				step1 = False
+				step2 = True
+				active = False
+			elif "module u0" in line:
+				active = True
+			elif active and "var" in line:
+				vcd_out.write(line)
+				orig_design_regs.add(line.split()[4])
+				refined_vars = refined_vars + [line.split()[3]]
+				
+						
+						
+## step 2: modules
+		
+		## assume a single relevant shadow_dut
+		
+		if step2:
+						
+			orig_design_regs = list(orig_design_regs)
+			orig_design_regs.sort()
+			
+			if "dumpvars" in line:
+				step2 = False
+				step3 = True
+				vcd_out.write("$enddefinitions $end\n$dumpvars\n#0\n")
+				active = False
+			elif "module" in line:
+				if "module shadow_dut" in line:
+					active = True
+				else:
+					active = False
+			elif active:
+				for reg in orig_design_regs:
+					if "shadow_" + reg + " " in line:
+						vcd_out.write(line)
+						refined_vars = refined_vars + [line.split()[3]]
+
+# step 3:
+		
+		if step3:
+		
+		## two line cases, two var cases
+
+			if "#" in line[0]:
+				if line.strip() != "#0":
+					cache = line
+					cache_live = True
+			else:
+				splits = line.strip().split()
+				if len(splits) == 1:
+					tag = line[1:].strip()
+				else:
+					tag = splits[1]
+				if tag in refined_vars:
+					if cache_live:
+						vcd_out.write(cache)
+						cache_live = False
+					vcd_out.write(line)
+
+refine("iACW")
