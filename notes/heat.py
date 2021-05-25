2e221724b83fc83f1b6243057408926ee55849a9
Calvin <cd-public@users.noreply.github.com>
2021-05-25 12:27:34 -0400

    Update and rename distro to heat.py

---
+++ notes/heat.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set()
+distro = [[3,2,12,4,7,5,10],
+[2,10,0,0,6,0,0],
+[2,2,12,5,4,11,10],
+[9,9,51,27,36,35,48],
+[6,23,20,10,27,15,17],
+[18,16,96,40,55,65,78],
+[14,25,68,31,69,53,64]]
+ax = sns.heatmap(distro)
+plt.title("Heatmap Flight Data")
+plt.show()
